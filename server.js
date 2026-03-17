76b91ecc275f8369d7faacf70b7e99260dbda3d9
Sukhil <sukhilgrandhisila@gmail.com>
2026-03-17 13:25:34 +0530

    port is added in server.js file

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use("/auth", userRoutes);
 app.use("/url", userMiddleware, urlRoutes);
 
-port = process.env.PORT;
+port = process.env.PORT || 8001;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
