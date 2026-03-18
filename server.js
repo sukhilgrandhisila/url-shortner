536e6fe1e70be7a05a63ae5ba89df7713dfe0f92
Sukhil <sukhilgrandhisila@gmail.com>
2026-03-18 17:01:03 +0530

    frontend url changed

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://furyurl.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
